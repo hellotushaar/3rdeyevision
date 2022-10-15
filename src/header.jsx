89baba509849393f3ed9ef0bcca11b56c1df0321
unknown <hellotushaar@gmail.com>
2022-10-15 14:13:38 +0530

    first commit

---
+++ src/header.jsx
@@ -0,0 +1,17 @@
+
+import './styles/header.css';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="">home</a>
+                <a href="">About</a>
+                <a href="">Services</a>
+                <a href="">Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
